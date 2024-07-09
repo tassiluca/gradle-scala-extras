9c9dc70480c9018c9453fff464610cd7741bb5c2
Luca Tassinari <luca.tassinari.2000@gmail.com>
2024-07-09 21:21:51 +0200

    test: test scalafix check task

--- src/test/resources/io/github/ltassi/scalaqa/custom/src/test/scala/MainTest.scala
+++ src/test/resources/io/github/ltassi/scalaqa/custom/src/test/scala/MainTest.scala
@@ -1,9 +1,7 @@
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import scala.Some
 
-class
-MainTest extends AnyFlatSpec with Matchers:
+class MainTest extends AnyFlatSpec with Matchers:
   "A trivial test" should "pass" in:
     println("Hello World Scala :)")
     true shouldBe true
