789e682802139a9f3b0cd902dc06a9c81455558f
Luca Tassinari <luca.tassinari.2000@gmail.com>
2024-07-10 17:59:19 +0200

    build: enable git hooks and automated scan

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("com.gradle.develocity") version "3.17.5"
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.7"
+}
+
+rootProject.name = "gradle-scala-qa"
+
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        uploadInBackground = !System.getenv("CI").toBoolean()
+        publishing.onlyIf { it.buildResult.failures.isNotEmpty() }
+    }
+}
+
+gitHooks {
+    preCommit {
+        tasks("check")
+    }
+    commitMsg { conventionalCommits() }
+    createHooks(overwriteExisting = true)
+}
