7fe9a446966c386702e12dc0ee99001a69d4b777
Luca Tassinari <luca.tassinari.2000@gmail.com>
2024-07-18 12:50:23 +0200

    build: maven repository is useless since all gradle plugins are retrieved thanks to gradlePluginPortal repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,16 +26,6 @@ val projectInfo = ProjectInfo()
 repositories {
     gradlePluginPortal()
     mavenCentral()
-    maven {
-        setOf(libs.gradle.scalafix)
-            .map { it.get() }
-            .map { "${it.group?.split(".")?.lastOrNull() ?: error("No group found for $it")}/${it.name}" }
-            .forEach { url = uri("https://maven.pkg.github.com/$it") }
-        credentials {
-            username = project.findProperty("gpr.user") as String? ?: System.getenv("GH_USERNAME")
-            password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
-        }
-    }
 }
 
 dependencies {
