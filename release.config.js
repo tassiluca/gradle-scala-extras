2a183adb8d40da0bf5d4207f5a88dfd78b3b6fa9
Luca Tassinari <luca.tassinari.2000@gmail.com>
2024-07-31 02:06:35 +0200

    ci: include skipped jobs in 'success' conditional rules

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 const publishCommands = `
-./gradlew uploadKotlinOSSRHToMavenCentralNexus uploadPluginMavenToMavenCentralNexus uploadPluginMarkerMavenToMavenCentralNexus releaseStagingRepositoryOnMavenCentral || exit 1
-./gradlew publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 2
+./gradlew publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 1
+./gradlew uploadKotlinOSSRHToMavenCentralNexus uploadPluginMavenToMavenCentralNexus uploadPluginMarkerMavenToMavenCentralNexus releaseStagingRepositoryOnMavenCentral || exit 2
 ./gradlew publishKotlinOSSRHPublicationToGitHubRepository publishPluginMavenPublicationToGitHubRepository || true
 `
 const releaseBranches = ["master"]
