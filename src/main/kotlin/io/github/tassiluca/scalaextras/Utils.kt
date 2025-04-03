041b2a0f11cf5adad0c46931e65ee8fd2f9ac779
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-04-03 14:28:18 +0200

    style: add editorconfig formatting rules

--- src/main/kotlin/io/github/tassiluca/scalaextras/Utils.kt
+++ src/main/kotlin/io/github/tassiluca/scalaextras/Utils.kt
@@ -12,11 +12,9 @@ internal fun File.createWithContent(source: String): File = parentFile
     .apply { mkdirs() }
     .also { if (!exists() || readText() != source) writeText(source) }
 
-internal fun Project.resolveOrFromResource(filename: String): String =
-    resolveOr(filename) { resource(filename) }
+internal fun Project.resolveOrFromResource(filename: String): String = resolveOr(filename) { resource(filename) }
 
-internal fun Project.resolveOrElse(filename: String, orElse: String): String =
-    resolveOr(filename) { orElse }
+internal fun Project.resolveOrElse(filename: String, orElse: String): String = resolveOr(filename) { orElse }
 
 private fun Project.resolveOr(filename: String, orElseStrategy: () -> String) =
     rootDir.resolve(filename).takeIf { it.exists() }?.readText() ?: orElseStrategy()
