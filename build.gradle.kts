7530620d118ad98000a4a67154c0f893841cf0e6
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-07-17 00:08:52 +0200

    fix(build): use api for scalafix and scalafmt dependencies, set for gradle properties and release max retries to 1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,21 @@
 import Utils.inCI
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 
 plugins {
     `java-gradle-plugin`
+    alias(libs.plugins.gitSemanticVersioning)
+    alias(libs.plugins.gradlePluginPublish)
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.qa)
     alias(libs.plugins.publishOnCentral)
-    alias(libs.plugins.gitSemanticVersioning)
-    alias(libs.plugins.gradlePluginPublish)
     alias(libs.plugins.multiJvmTesting)
 }
 
 group = "io.github.tassiluca"
 description = "A plugin to enhance the Scala gradle core plugin with quality assurance and common useful tools"
-inner class ProjectInfo {
+class ProjectInfo {
     val repoOwner = "tassiluca"
     val longName = "Gradle Scala Extras Plugin"
     val website = "https://github.com/$repoOwner/$name"
@@ -42,12 +43,18 @@ multiJvm {
 dependencies {
     api(gradleApi())
     api(gradleKotlinDsl())
-    implementation(kotlin("stdlib-jdk8"))
-    implementation(libs.gradle.scalafmt)
-    implementation(libs.gradle.scalafix)
+    api(libs.gradle.scalafmt)
+    api(libs.gradle.scalafix)
     testImplementation(gradleTestKit())
     testImplementation(libs.bundles.kotlin.testing)
     testImplementation(libs.gradle.plugins.testkit)
+}
+
+tasks.withType<KotlinCompilationTask<*>>().configureEach {
+    compilerOptions {
+        allWarningsAsErrors = true
+        freeCompilerArgs = listOf("-opt-in=kotlin.RequiresOptIn")
+    }
 }
 
 tasks.withType<Test>().configureEach {
@@ -76,31 +83,17 @@ signing {
     }
 }
 
-gradlePlugin {
-    plugins {
-        website = projectInfo.website
-        vcsUrl = projectInfo.vcsUrl
-        create("") {
-            id = "$group.${project.name}"
-            displayName = projectInfo.longName
-            description = project.description
-            implementationClass = projectInfo.pluginImplementationClass
-            tags = projectInfo.tags
-        }
-    }
-}
-
 publishOnCentral {
-    projectLongName = projectInfo.longName
-    projectDescription = description
-    projectUrl = projectInfo.website
-    scmConnection = projectInfo.scm
-    licenseName = "Apache License, Version 2.0"
-    licenseUrl = "https://www.apache.org/licenses/LICENSE-2.0"
-    repoOwner = projectInfo.repoOwner
+    projectLongName.set(projectInfo.longName)
+    projectDescription.set(description)
+    projectUrl.set(projectInfo.website)
+    scmConnection.set(projectInfo.scm)
+    licenseName.set("Apache License, Version 2.0")
+    licenseUrl.set("https://www.apache.org/licenses/LICENSE-2.0")
+    repoOwner.set(projectInfo.repoOwner)
     repository("https://maven.pkg.github.com/${projectInfo.repoOwner}/${rootProject.name}", "github") {
-        user = repoOwner
-        password = System.getenv("GITHUB_TOKEN")
+        user.set(repoOwner)
+        password.set(System.getenv("GITHUB_TOKEN"))
     }
 }
 
@@ -110,11 +103,25 @@ publishing {
             pom {
                 developers {
                     developer {
-                        name = "Luca Tassinari"
-                        email = "luca.tassinari.2000@gmail.com"
+                        name.set("Luca Tassinari")
+                        email.set("luca.tassinari.2000@gmail.com")
                     }
                 }
             }
+        }
+    }
+}
+
+gradlePlugin {
+    plugins {
+        website.set(projectInfo.website)
+        vcsUrl.set(projectInfo.vcsUrl)
+        create("") {
+            id = "$group.${project.name}"
+            displayName = projectInfo.longName
+            description = project.description
+            implementationClass = projectInfo.pluginImplementationClass
+            tags.set(projectInfo.tags)
         }
     }
 }
